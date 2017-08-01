5c23ce91e32fa885c60949046bb84299be79541d
Jennifer K <you@example.com>
2017-08-01 15:10:16 +0800

    first commit

---
+++ app/src/main/java/com/example/emily/beaconside/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.emily.beaconside;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+
+    Button side_new;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side_bar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        side_new = (Button)findViewById(R.id.side_new);
+
+
+
+        findViewById(R.id.side_group_bt).setOnClickListener(new View.OnClickListener() {//group
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.side_class_ls).setVisibility(View.GONE);
+                findViewById(R.id.side_group_ls).setVisibility(View.VISIBLE);
+                findViewById(R.id.side_group_bt).setBackgroundDrawable(getResources().getDrawable(R.drawable.my_group_l));
+                findViewById(R.id.side_class_bt).setBackgroundDrawable(getResources().getDrawable(R.drawable.my_class));
+                side_new.setText("+ New group");
+//                Toast.makeText(MainActivity.this, "Button1 Click!", Toast.LENGTH_SHORT).show();
+//                onBackPressed();
+            }
+        });
+        findViewById(R.id.side_class_bt).setOnClickListener(new View.OnClickListener() {//class
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.side_group_ls).setVisibility(View.GONE);
+                findViewById(R.id.side_class_ls).setVisibility(View.VISIBLE);
+                findViewById(R.id.side_group_bt).setBackgroundDrawable(getResources().getDrawable(R.drawable.my_group));
+                findViewById(R.id.side_class_bt).setBackgroundDrawable(getResources().getDrawable(R.drawable.my_class_l));
+                side_new.setText("+ New classification");
+//                Toast.makeText(MainActivity.this, "Button2 Click!", Toast.LENGTH_SHORT).show();
+//                onBackPressed();
+            }
+        });
+
+
+    }
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+}
---
+++ app/src/main/java/com/example/emily/beaconside/rowdata.java
@@ -0,0 +1,86 @@
+package com.example.emily.beaconside;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class rowdata extends BaseAdapter {
+    private final Context context;
+    private final String[] value_deviceName;
+    private final String[] value_deviceDsc;
+
+    private LayoutInflater mInflater;
+
+    public rowdata(Context context, String[] values, String[] values2) {//架構子
+        mInflater = LayoutInflater.from(context);//傳入Activity
+        this.context = context;
+        this.value_deviceName = values;
+        this.value_deviceDsc = values2;
+    }
+
+    @Override
+    public int getCount() {//算device Name長度
+        return value_deviceName.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;//緩存
+        if(convertView==null){
+            convertView=mInflater.inflate(R.layout.activity_rowdata,null);//inflate(要加載的佈局id，佈局外面再嵌套一層父佈局(root)->如果不需要就寫null)
+            holder = new ViewHolder();
+
+            holder.beaconImage = (ImageView) convertView
+                    .findViewById(R.id.beaconImage);
+            holder.beaconName = (TextView) convertView
+                    .findViewById(R.id.beaconName);
+            holder.beaconDistance = (TextView) convertView
+                    .findViewById(R.id.beaconDistance);
+
+            convertView.setTag(holder);//把查找的view通過ViewHolder封裝好緩存起來方便 ​​多次重用，當需要時可以getTag拿出來
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        /* 換圖片 */
+        if(position%2==0){
+            holder.beaconImage.setImageResource(R.drawable.beacon);   // 換圖片
+        }else{
+            holder.beaconImage.setImageResource(R.drawable.beacon);   // 換圖片
+        }
+
+        holder.beaconName.setText(value_deviceName[position]);
+        holder.beaconDistance.setText(value_deviceDsc[position]);
+        return convertView;
+    }
+
+    static class ViewHolder {//緩存用。一種設計方法，就是設計個靜態類，緩存一下，省得Listview更新的時候，還要重新操作。
+        ImageView beaconImage;
+        TextView beaconName;
+        TextView beaconDistance;
+        TextView beaconNearby;
+        Button item_setting;
+    }
+}
