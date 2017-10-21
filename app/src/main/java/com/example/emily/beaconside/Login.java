ba1c10ef8487ce18095f8f8dcc3db84ace58c046
zizisugar <emily971133@gmail.com>
2017-10-21 10:34:05 +0800

    oao?

--- app/src/main/java/com/example/emily/beaconside/Login.java
+++ app/src/main/java/com/example/emily/beaconside/Login.java
@@ -1,15 +1,19 @@
 package com.example.emily.beaconside;
 
-import android.app.Application;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SimpleAdapter;
@@ -38,11 +42,13 @@ import com.facebook.login.widget.LoginButton;
 import com.facebook.share.ShareApi;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
+import android.widget.ImageView;
+import com.example.emily.beaconside.R;
 
-import static java.lang.Integer.parseInt;
-
+import static com.example.emily.beaconside.R.id.toolbar;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
+
 
     CallbackManager callbackManager;
     AccessTokenTracker accessTokenTracker;
@@ -63,6 +69,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
 
 
         callbackManager = CallbackManager.Factory.create();
@@ -489,4 +496,10 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 //            search();
 //        }
     }
+
+
+
+
+
+
 }
